ecf6efcd15b94f3af74a32af78f741c25e188925
Kuncahyo Setyo Nugroho <ksnugroho26@gmail.com>
2020-04-15 22:44:08 +0700

    hi, lysa

---
+++ hi-lysa/index.php
@@ -0,0 +1,163 @@
+<?php
+date_default_timezone_set("Asia/Jakarta"); // default time
+
+function salam(){ // fuction salam
+	$time = date("H");
+	$timezone = date("e");
+	if ($time < "12") {
+			echo "Selamat Pagi";
+	} else if ($time >= "12" && $time < "17") {
+			echo "Selamat Siang";
+	} else if ($time >= "17" && $time < "19") {
+			echo "Selamat Sore";
+	} else if ($time >= "19") {
+			echo "Selamat Malam";
+	}
+}
+
+function pesan(){ // fuction salam
+	$time = date("H");
+	$timezone = date("e");
+	if ($time < "12") {
+			echo "Semangat pagi menyambut hari baru";
+	} else if ($time >= "12" && $time < "17") {
+			echo "Mager time, Movie time";
+	} else if ($time >= "17" && $time < "19") {
+			echo "Waktunya olahraga biar sehat.";
+	} else if ($time >= "19") {
+			echo "Cepat tidur, jangan lupa gosok gigi kak";
+	}
+}
+?>
+
+<script type="text/javascript">
+function date_time(id)
+{
+		date = new Date;
+		year = date.getFullYear();
+		month = date.getMonth();
+		months = new Array('Januari', 'Februari', 'Maret', 'April', 'Mae', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember');
+		d = date.getDate();
+		day = date.getDay();
+		days = new Array('Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu');
+		h = date.getHours();
+		if(h<10)
+		{
+						h = "0"+h;
+		}
+		m = date.getMinutes();
+		if(m<10)
+		{
+						m = "0"+m;
+		}
+		s = date.getSeconds();
+		if(s<10)
+		{
+						s = "0"+s;
+		}
+		// result = ''+days[day]+' '+d+' '+months[month]+' '+year+' '+h+':'+m+':'+s;
+		document.getElementById(id).innerHTML = ''+h+':'+m+':'+s+' <br/> '+days[day]+' , '+d+' '+months[month]+' '+year;
+		setTimeout('date_time("'+id+'");','1000');
+		return true;
+	}
+</script>
+
+<!DOCTYPE HTML>
+<!--
+	Identity by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Hi, Lysa ! 💦</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="shortcut icon" href="images/leaf.png">
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Main -->
+					<section id="main">
+						<header>
+							<span class="avatar"><img src="images/avatar.jpg" alt="" /></span>
+							<p>
+								<span id="date_time"></span>
+								<script type="text/javascript">window.onload = date_time('date_time');</script>
+							</p>
+							<h1>Hi, Lysa ! <br> <?php salam() ?></h1>
+							<p><?php pesan() ?></p>
+
+						</header>
+<!--
+						<hr />
+						<h2>Extra Stuff!</h2>
+						<form method="post" action="#">
+							<div class="fields">
+								<div class="field">
+									<input type="text" name="name" id="name" placeholder="Name" />
+								</div>
+								<div class="field">
+									<input type="email" name="email" id="email" placeholder="Email" />
+								</div>
+								<div class="field">
+									<select name="department" id="department">
+										<option value="">Department</option>
+										<option value="sales">Sales</option>
+										<option value="tech">Tech Support</option>
+										<option value="null">/dev/null</option>
+									</select>
+								</div>
+								<div class="field">
+									<textarea name="message" id="message" placeholder="Message" rows="4"></textarea>
+								</div>
+								<div class="field">
+									<input type="checkbox" id="human" name="human" /><label for="human">I'm a human</label>
+								</div>
+								<div class="field">
+									<label>But are you a robot?</label>
+									<input type="radio" id="robot_yes" name="robot" /><label for="robot_yes">Yes</label>
+									<input type="radio" id="robot_no" name="robot" /><label for="robot_no">No</label>
+								</div>
+							</div>
+							<ul class="actions special">
+								<li><a href="#" class="button">Get Started</a></li>
+							</ul>
+						</form>
+						<hr />
+						 -->
+						<footer>
+							<ul class="icons">
+								<!-- <li><a href="#" class="icon brands fa-twitter">Twitter</a></li> -->
+								<li><a href="https://www.instagram.com/lysaascarya/" class="icon brands fa-instagram">Instagram</a></li>
+								<!-- <li><a href="#" class="icon brands fa-facebook-f">Facebook</a></li> -->
+							</ul>
+						</footer>
+					</section>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<ul class="copyright">
+							<li>Made With ❤️ Cahyo</li><br>
+							Template: <a href="https://html5up.net/" target="_blank">HTML5 UP</a>
+							<li>Image: <a href="https://unsplash.com/" target="_blank">Unsplash </li><li></li>
+						</ul>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script>
+				if ('addEventListener' in window) {
+					window.addEventListener('load', function() { document.body.className = document.body.className.replace(/\bis-preload\b/, ''); });
+					document.body.className += (navigator.userAgent.match(/(MSIE|rv:11\.0)/) ? ' is-ie' : '');
+				}
+			</script>
+
+	</body>
+</html>
